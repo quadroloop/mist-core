342dd2c569a3e76325d6392698e21a460f819b5a
Bryce Mercines <nplixel@gmail.com>
2020-04-28 04:23:33 +0800

    return error for failure of reading file

--- mist.go
+++ mist.go
@@ -22,13 +22,13 @@ func scanDir() {
 		file, err := os.Open(f.Name())
 
 		if err != nil {
-			 fmt.Println("Error!")
+			 fmt.Println(err)
 		}
 		defer file.Close()
 
 		fi, err := file.Stat()
 		if err != nil {
-			fmt.Println("Error!")
+			fmt.Println(err)
 		}
 		if fi.IsDir() {
 				fmt.Println("DIRECTORY===>",f.Name())
