053948f726617c4abb56feb56f23edb9b299bbbe
Bryce Mercines <nplixel@gmail.com>
2020-04-29 04:00:43 +0800

    refactored scanDir function , converted to scanNodes and added mapNode function

--- mist.go
+++ mist.go
@@ -9,11 +9,12 @@ import (
 
 func main() {
 	fmt.Println("Running mist...")
-	scanDir()
+	scanNodes()
 }
 
-func scanDir() {
-	files, err := ioutil.ReadDir("./")
+func scanNodes() {
+	files, err := ioutil.ReadDir("./nodes")
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,18 +23,18 @@ func scanDir() {
 		file, err := os.Open(f.Name())
 
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Read file error 1:",err)
 		}
+
 		defer file.Close()
 
-		fi, err := file.Stat()
-		if err != nil {
-			fmt.Println(err)
-		}
-		if fi.IsDir() {
-			fmt.Println("DIRECTORY===>", f.Name())
-		} else {
-			fmt.Println("FILE===>", f.Name())
+		if fi, err := file.Stat(); err != nil || fi.IsDir() {
+			mapNode(f.Name())
 		}
 	}
+}
+
+
+func mapNode(node string) {
+	fmt.Println("Mapping Node...",node)
 }
